174738ec877a54201f20770300a95037c6ebf293
M1dnight <108243683+M1dnight-ofcl@users.noreply.github.com>
2024-07-03 16:18:02 -0400

    finished. rather ez compared to the rest of the shit i gotta do

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import { useEffect } from 'react';
+import './style/App.css';
+import { useGlitch } from 'react-powerglitch'
+const App=()=>{
+  document.title="Beansite Selector"
+  document.getElementById("icon").setAttribute("href","/assets/favicon_modern.png");
+  const OS=(props)=>{
+    const glitch = useGlitch({
+      "timing": {
+        "duration": 1000
+      },
+      "glitchTimeSpan": {
+        "start":0,
+        "end":1
+      },
+      "shake": {
+        "velocity": 5,
+        "amplitudeX": 0.05,
+        "amplitudeY": 0.05
+      }
+    })
+    useEffect(()=>{
+      document.getElementById(props.id).addEventListener("click",()=>{
+        if(props.glitch){
+          const confirmation=confirm("This version of beansite is abandoned and buggy. are you sure you want to continue?");
+          if(confirmation){
+            window.location.href=props.url;}
+        }else{window.location.href=props.url;}
+      });
+    },[]);
+    return(<div className="os" id={props.id}>
+      {!props.glitch?
+        <div className='hdd_icon'></div>:
+        <span>
+          <div className='hdd_icon' id="glitched" ref={glitch.ref}></div>
+        </span>}
+      <h1 id={`${props.id}txt`}>{props.title}</h1>
+    </div>)
+  }
+  return (<>
+    <div id="os_wrapper">
+      <OS id="b95" title="Beansite 95" url="https://mb95.vercel.app"/>
+      <OS id="b98" title="Beansite 98 (preview)" url="https://b98-preview.vercel.app" glitch={true}/>
+      <OS id="bxp" title="Beansite XP" url="https://mbxp.vercel.app"/>
+    </div>
+  </>)
+}
+export default App;
